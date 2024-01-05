f7449993cc2b36a0eddb8bcf1b02cc7a0d7cdc3d
Ozlem <ozbasiozlem@gmail.com>
2024-01-05 05:55:09 +0300

    Cucumber Parallel-1

---
+++ src/test/java/Runners/_08_TestRunnerParallel.java
@@ -0,0 +1,33 @@
+package Runners;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+@CucumberOptions(
+
+        tags = "@Regression",       //  Hangi senaryolarda bu etiket varsa o senaryolar çalıstırılacak
+        features = {"src/test/java/FeatureFiles/" },
+        glue = {"StepDefinitions"},
+        plugin = { "html:target//cucumber-reports.html" }
+
+)
+
+public class _08_TestRunnerParallel extends AbstractTestNGCucumberTests {
+
+
+    @BeforeClass
+    @Parameters("browser")
+    public void beforeClass (String browser)
+    {
+        // Burada browser set edilecek
+
+
+
+    }
+
+
+
+}
